b7766697ff978a13c86fa711c10e2d0fcd1857d6
al21011 <al21011@shibaura-it.ac.jp>
2024-05-31 13:09:31 +0900

    xmlファイル容量削減コードの追加

---
+++ extractionXml.py
@@ -0,0 +1,25 @@
+'''
+ヘルスケアから書き出したxmlファイルが重いので心拍数のみのファイルに書き換えるソースコードです
+'''
+
+# 対象ファイルのパスを入力
+fileName = "xmlDataFile/export5_28.xml"
+# 必要なデータのRecode type名を入力
+recodeType = r'"HKQuantityTypeIdentifierHeartRate"'
+
+# Recode type名を含む行の抽出
+f = open(fileName, "r")
+newData = ""
+while True:
+    line = f.readline()
+    if line:
+        if recodeType in line:
+            newData += line
+    else:
+        break
+f.close()
+
+# 抽出したデータをファイルへ書き込み
+f = open(fileName, "w")
+f.write(newData)
+f.close()

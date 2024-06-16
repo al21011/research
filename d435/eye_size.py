272c0a381f49ad99569a26aa225b95b5abdcc771
al21011 <al21011@shibaura-it.ac.jp>
2024-06-17 02:19:03 +0900

    目の開き具合を見るコードの追加

--- d435/eye_size.py
+++ d435/eye_size.py
@@ -1,6 +1,21 @@
 import pyrealsense2 as rs
 import numpy as np
 import cv2
+import dlib
+from imutils import face_utils
+from scipy.spatial import distance
+
+def calc_ear(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+    eye_ear = (A + B) / (2.0 * C)
+    return round(eye_ear, 3)
+
+def eye_marker(face_mat, position):
+    for i, ((x, y)) in enumerate(position):
+        cv2.circle(face_mat, (x, y), 1, (255, 255, 255), -1)
+        cv2.putText(face_mat, str(i), (x + 2, y - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
 
 # RealSenseカメラの設定
 pipeline = rs.pipeline()
@@ -9,7 +24,9 @@ config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
 # カスケード分類器の読み込み
 cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+# eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+parts_detector = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
 
 # RealSenseカメラのストリーミング開始
 pipeline.start(config)
@@ -31,28 +48,48 @@ try:
         # 顔の検出
         faces = cascade.detectMultiScale(gray, scaleFactor=1.11, minNeighbors=3, minSize=(100, 100))
 
-        for (x, y, w, h) in faces:
-            # 顔を矩形で囲む
-            cv2.rectangle(color_image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    # 以下引用
+        tick = cv2.getTickCount()
 
-            # 顔の上半分を検出対象範囲とする
-            eyes_gray = gray[y : y + int(h/2), x : x + w]
-            eyes = eye_cascade.detectMultiScale(eyes_gray, scaleFactor=1.11, minNeighbors=3, minSize=(8, 8))
+        if len(faces) == 1:
+            x, y, w, h = faces[0, :]
+            cv2.rectangle(color_image, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-            for (ex, ey, ew, eh) in eyes:
-                # 目を矩形で囲む
-                cv2.rectangle(color_image, (x + ex, y + ey), (x + ex + ew, y + ey + eh), (255, 255, 0), 1)
+            face_gray = gray[y :(y + h), x :(x + w)]
+            scale = 480 / h
+            face_gray_resized = cv2.resize(face_gray, dsize=None, fx=scale, fy=scale)
 
-            if len(eyes) == 0:
-                # 目が閉じているときの警告を表示
-                cv2.putText(color_image, "Blink!", (10, 100), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 2, cv2.LINE_AA)
+            face = dlib.rectangle(0, 0, face_gray_resized.shape[1], face_gray_resized.shape[0])
+            face_parts = parts_detector(face_gray_resized, face)
+            face_parts = face_utils.shape_to_np(face_parts)
 
-        # 画像を表示
-        cv2.imshow('RealSense', color_image)
+            left_eye = face_parts[42:48]
+            eye_marker(face_gray_resized, left_eye)
 
-        # キー入力を待機し、ESCキーでループを抜ける
+            left_eye_ear = calc_ear(left_eye)
+            cv2.putText(color_image, "LEFT eye EAR:{} ".format(left_eye_ear), 
+                (10, 100), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1, cv2.LINE_AA)
+
+            right_eye = face_parts[36:42]
+            eye_marker(face_gray_resized, right_eye)
+
+            right_eye_ear = calc_ear(right_eye)
+            cv2.putText(color_image, "RIGHT eye EAR:{} ".format(round(right_eye_ear, 3)), 
+                (10, 120), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1, cv2.LINE_AA)
+
+            if (left_eye_ear + right_eye_ear) < 0.55:
+                cv2.putText(color_image,"Sleepy eyes. Wake up!",
+                    (10,180), cv2.FONT_HERSHEY_PLAIN, 3, (0,0,255), 3, 1)
+
+            cv2.imshow('frame_resize', face_gray_resized)
+
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - tick)
+        cv2.putText(color_image, "FPS:{} ".format(int(fps)), 
+            (10, 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2, cv2.LINE_AA)
+
+        cv2.imshow('frame', color_image)
         if cv2.waitKey(1) == 27:
-            break
+            break  # esc to quit
 
 finally:
     # ストリーミング停止

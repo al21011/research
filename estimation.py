1df4527df8b231bf1752d8829c38a82ed94cde45
ryota <al21011@shibaura-it.ac.jp>
2024-10-17 03:44:22 +0900

    予測機能実装の準備

---
+++ estimation.py
@@ -0,0 +1,11 @@
+'''
+とりあえずは長谷川さんが開発に取り組めるよう色んな形で値を算出して送るところを実装します。
+このファイルでは(緊張感、集中力)のfloat型の値を返します。
+'''
+
+import random
+
+# ランダムな値を返す
+def random_value(min_value, max_value):
+    return random.uniform(min_value, max_value)
+
